1b37876d375e3051dd828554190ab08d5c36a213
David <david-hpf@hotmail.com>
2023-05-26 11:03:00 +0200

    cleanup

--- web/main.js
+++ web/main.js
@@ -4,11 +4,11 @@ import { auth } from "./app/firebase.js"
 onAuthStateChanged(auth, async (user) => {
     console.log(user)
     if (user) {
-        if (window.location.pathname !== '/dashboard.html') {
+        if (window.location.pathname !== '/src/dashboard.html') {
             window.location.href = "dashboard.html"
         }
     } else {
-        if (window.location.pathname !== '/index.html') {
+        if (window.location.pathname !== '/src/index.html') {
             window.location.href = "index.html";
         }
     }
