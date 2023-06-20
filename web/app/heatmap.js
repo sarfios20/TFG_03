95df13f09d2e7a9e379dfd91f720a218f387df06
David <david-hpf@hotmail.com>
2023-06-20 11:01:24 +0200

    aded map

--- web/app/heatmap.js
+++ web/app/heatmap.js
@@ -1,1 +1,13 @@
 console.log('heatmap.js loaded');
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+// Attach initMap to the window object
+window.initMap = initMap;
